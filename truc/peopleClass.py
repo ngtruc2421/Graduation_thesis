766bf265ff6857b0891c73e37503bc4d930fb806
ngtruc2421 <tt1799917@gmail.com>
2024-04-23 16:41:59 +0700

    basic python, json

---
+++ truc/peopleClass.py
@@ -0,0 +1,14 @@
+class people:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    
+    def profile(setf):
+        print("Name: "+setf.name)
+        print("Age: "+setf.age)
+
+    def eat(setf):
+        print(setf.name+" is eating the noodles.")
+    
+    def sleep(setf):
+        print(setf.name+" is sleeping")
