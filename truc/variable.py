f5e3e0eed0763a9c9c0112c25075ee2368097b01
ngtruc2421 <tt1799917@gmail.com>
2024-05-07 11:58:50 +0700

    update skelethon and delete truc dirictory

--- truc/variable.py
+++
@@ -1,5 +0,0 @@
-URL=" https://bearstore-testsite.smartbear.com/"
-LIST__ANIMAL=["Cat", "Dog", "Fish", "Mouse", "Elephant"]
-ITEM_PRO="css:article[data-id='12']"
-RADIO_PRICE="css:.facet-group[data-key='price']"
-RADIO_PRICE_ITEM="css:span[class='facet-label has-count']"
